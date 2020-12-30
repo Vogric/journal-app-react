2c7c09a414f2b1f34abba8bd1c8cafd9ba616818
vogric <blvogric@gmail.com>
2020-12-30 17:05:56 -0300

    Configuring Redux
    * React Redux was installed
    * Folders reducers and store were created
    * authReducer was created
    * store was created

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { AppRouter } from "./Routers/AppRouter";
+import { store } from "./store/store";
 
 export const JournalApp = () => {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />;
+    </Provider>
+  );
 };
