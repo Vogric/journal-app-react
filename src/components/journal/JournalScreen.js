2d4af26a579927f0c4dad623f1412ae25d457e0b
vogric <blvogric@gmail.com>
2020-12-30 16:08:42 -0300

    NotesAppBar was created

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NoteScreen } from "../notes/NoteScreen";
 import { NothingSelected } from "./NothingSelected";
 import { Sidebar } from "./Sidebar";
 
@@ -7,7 +8,8 @@ export const JournalScreen = () => {
     <div className="journal__main-content">
       <Sidebar />
       <main>
-        <NothingSelected />
+        {/*         <NothingSelected /> */}
+        <NoteScreen />
       </main>
     </div>
   );
