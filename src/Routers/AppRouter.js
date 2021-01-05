903132d2196f07c63147411bfce816a725d085b0
vogric <blvogric@gmail.com>
2021-01-04 22:37:02 -0300

    Authentication state persistence on reload

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,8 +7,21 @@ import {
 } from "react-router-dom";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { firebase } from "../firebase/firebaseConfig";
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <Router>
       <Switch>
