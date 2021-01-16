67e0cadebfc4cbb613379f60a7fc36d7157573bf
vogric <blvogric@gmail.com>
2021-01-16 18:24:36 -0300

    The static name in the sidebar has been replaced with the name of the person who is logged in.

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { JournalEntries } from "./JournalEntries";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { startLogout } from "../../actions/auth";
 
 export const Sidebar = () => {
   const dispatch = useDispatch();
+
+  const { name } = useSelector((state) => state.auth);
 
   const handleLogout = () => {
     dispatch(startLogout());
@@ -15,7 +17,7 @@ export const Sidebar = () => {
       <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
           <i className="far fa-moon" />
-          <span> Braian</span>
+          <span> {name} </span>
         </h3>
         <button onClick={handleLogout} className="btn">
           Logout
